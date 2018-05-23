f30b7f9a2212678bbecc8a8afa077127e5a80e75
Усманаев Андрей <andrei.usmanaev@gmail.com>
2018-05-23 14:02:32 +0300

    xmlserialization

--- WpfApplication1/WpfApplication1/MainWindow.xaml.cs
+++ WpfApplication1/WpfApplication1/MainWindow.xaml.cs
@@ -12,14 +12,25 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
-
+using System.IO;
+using System.Xml.Serialization;
+using System.Xml;
 namespace WpfApplication1 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window {
+        public List<Meme> list = new List<Meme>();
         public MainWindow() {
             InitializeComponent();
+            Meme m = new Meme();
+            m.Source = new Uri(@"http://i0.kym-cdn.com/entries/icons/original/000/016/546/hidethepainharold.jpg");
+            m.Answers.Add("Harold");
+            XmlSerializer s = new XmlSerializer(typeof(Meme));
+            using (StreamWriter sw = new StreamWriter(@"C:\Users\Admin\Source\Repos\memegame\WpfApplication1\WpfApplication1\memeBase.xml"))
+            {
+                s.Serialize(sw, list);
+            }
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
