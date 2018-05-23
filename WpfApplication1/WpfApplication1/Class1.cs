c8667e45cc959b066a34a63a5290a026aa59e2a1
Усманаев Андрей <andrei.usmanaev@gmail.com>
2018-05-23 13:43:30 +0300

    added meme class

--- WpfApplication1/WpfApplication1/Class1.cs
+++ WpfApplication1/WpfApplication1/Class1.cs
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 namespace WpfApplication1 {
     public class Meme {
 
-        public string Source { set; get; }
+        public Uri Source { set; get; }
         public List<string> Answers = new List<string>();
 
     }
