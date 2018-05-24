f5d0fbee495aeb8670162183b2aefdfd47d7f501
martyniukroman <hjbfrcgq@gmail.com>
2018-05-24 13:51:36 +0300

    LinkToMeme fixed

--- WpfApplication1/WpfApplication1/MainWindow.xaml.cs
+++ WpfApplication1/WpfApplication1/MainWindow.xaml.cs
@@ -110,6 +110,8 @@ namespace WpfApplication1
 
             int temp = randomMeme;
 
+            System.Diagnostics.Process.Start(list[randomMeme].LinkToMeme);
+
             while (true) {
                 if (randomMeme == temp) {
                     randomMeme = rand.Next(0, list.Count);
@@ -135,7 +137,7 @@ namespace WpfApplication1
                 totalTriesLabel.Content = "Total tries: " + totalTries.ToString();
                 correctAnswersLabel.Content = "Correct answers: " + correctAnswers.ToString();
 
-            System.Diagnostics.Process.Start(list[temp].LinkToMeme);
+            
 
         }
 
